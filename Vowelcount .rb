cd696ae8f6104faf38b01ebc9c634427d0ef41eb
Melinda Batang <meeelinda.b@gmail.com>
2021-08-18 09:39:50 +1000

    update comments

--- Vowelcount .rb
+++ Vowelcount .rb
@@ -1,8 +1,8 @@
-#Return the number (count) of vowels in the given string.
+#R eturn the number (count) of vowels in the given string.
 
-#We will consider a, e, i, o, u as vowels for this Kata (but not y).
+# We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
-#he input string will only consist of lower case letters and/or spaces.
+# The input string will only consist of lower case letters and/or spaces.
 
 def getCount(inputStr)
   inputStr.count("aeiou")
