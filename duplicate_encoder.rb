83d762904e47fb1b4f238ca6ffd59583a7f38cc8
Melinda Batang <meeelinda.b@gmail.com>
2021-08-18 10:29:21 +1000

    use downcase, chars, map and join

---
+++ duplicate_encoder.rb
@@ -0,0 +1,22 @@
+# The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+# Examples
+
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))(("
+
+
+# Better Solution
+def duplicate_encode(word)
+  word
+      .downcase
+      .chars
+      .map { |char| word.downcase.count(char) > 1 ? letter = ')' : letter = '(' }
+      .join
+end
+
+# Another Solution
+def duplicate_encode(word)
+  word.downcase.chars.map {|x| word.downcase.count(x) > 1 ? ")" : "("}.join("")
+end
